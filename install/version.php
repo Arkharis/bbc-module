7889fae1c687c9a86265fff8cec36b6d9cfa71e6
AA.Batashev <AA.Batashev@npo-nauka.ru>
2020-09-24 10:05:58 +0300

    Update version

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    'VERSION' 		=> '0.0.1',
-    'VERSION_DATE' 	=> '2015-04-06 21:45:00'
+    'VERSION' => '1.0.1',
+    'VERSION_DATE' => '2020-09-24 10:06:00'
 );
